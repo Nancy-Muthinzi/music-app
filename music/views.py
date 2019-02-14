e0579aefeea83af1fd6feb1e6e03d08c4afc259e
Nancy-Muthinzi <nanciekathini@gmail.com>
2019-02-15 00:26:45 +0300

    add bootstrap cdn link

--- music/views.py
+++ music/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Welcome to the Music App homepage</h1>')
+    all_albums = Album.objects.all()
+
+    return render(request, 'music/index.html', {'all_albums':all_albums})
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+ 
+    return render(request, 'music/detail.html', {'album':album})
+
+def favourite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):    
+        return render(request, 'music/detail.html', {'album':album, 'error_message': "You did not select a valid song!"})    
+    else:
+        selected_song.is_favourite = True
+        selected_song.save()   
+
+    return render(request, 'music/detail.html', {'album':album})
+     
